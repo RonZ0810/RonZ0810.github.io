0e8d274c294be329862ed6b731719219d75269d4
Ronald.Zhang <Ronald.Zhang@demolab.jolera.com>
2025-08-27 09:26:15 -0400

    Revamped homepage

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const menuBtn = document.getElementById("menuBtn");
+  const sidebar = document.getElementById("sidebar");
+
+  menuBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("open");
+  });
+});
