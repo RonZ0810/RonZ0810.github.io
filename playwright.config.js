0d697268431a6085fb376857e8ae36e245bedbfa
Ronald Zhang <ronzhang0810@gmail.com>
2026-07-11 13:00:19 -0400

    pinball V2

--- playwright.config.js
+++ playwright.config.js
@@ -2,7 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  timeout: 30_000,
+  timeout: 60_000,
   fullyParallel: true,
   workers: 2,
   reporter: [['list']],
